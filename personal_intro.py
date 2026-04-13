a685ae3e168f2a83c69f1dc3ba3b254466d4a111
RubeenaMS <rubeenams011@gmail.com>
2026-04-13 15:07:15 +0100

    Add files via upload

---
+++ personal_intro.py
@@ -0,0 +1,9 @@
+name=input('Enter your name : ')
+age=int(input('Enter your age :'))
+hobby=input('Enter your hobby :')
+sub=input('Enter your favorite coding language : ')
+welcome_message= f'''Hi {name}, Welcome!
+-------------------------------------------------------------------------
+Nice to meet {age} years old {name} who really enjoys {hobby} with {sub}
+--------------------------------------------------------------------------'''
+print(welcome_message)
